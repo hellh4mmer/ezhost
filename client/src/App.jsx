b548832360a52a6b7b62846d7d5235721e365d27
c4lyp5o <calypso@calypsocloud.one>
2025-10-15 23:57:38 +0800

    fix: use window origin for file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -181,7 +181,7 @@ function App() {
 						<div className="flex justify-center gap-4 mt-4">
 							{!selectedFile.isDirectory && (
 								<a
-									href={`/${selectedFile.url}`}
+									href={`${window.location.origin}/${selectedFile.url}`}
 									target="_blank"
 									rel="noopener noreferrer"
 									className="px-4 py-2 bg-purple-600 rounded-md text-white text-sm hover:bg-purple-500 transition"
