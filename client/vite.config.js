82893878aa5b642eda61ffdfd9202bcb12ce74e2
c4lyp5o <calypso@calypsocloud.one>
2025-10-15 23:38:05 +0800

    fix: out dir to dist, proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,36 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // port 3000
-  server: {
-    port: 3000,
-  },
-  // /api to http://localhost:5000/api
-  proxy: {
-    '/api': 'http://localhost:5000/api',
-    '/static': 'http://localhost:5000/static',
-  },
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-    tailwindcss(),
-  ],
+	server: {
+		// port 3000
+		port: 3000,
+		// /api to http://localhost:5000/api
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+				secure: false,
+			},
+			"/static": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+	plugins: [
+		react({
+			babel: {
+				plugins: [["babel-plugin-react-compiler"]],
+			},
+		}),
+		tailwindcss(),
+	],
+	build: {
+		outDir: "../dist",
+		emptyOutDir: true,
+	},
 });
